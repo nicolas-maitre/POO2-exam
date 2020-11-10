27942a7b3314a2bdcadba06db0c475f31a67a2fa
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2020-11-10 16:04:29 +0100

    parsers using factory dp

--- exam.rb
+++ exam.rb
@@ -2,6 +2,7 @@
 # Examen POO2
 #
 require_relative 'text_providers'
+require_relative 'html_links_parsers'
 require_relative 'link_displayers'
 
 parse_method = ARGV[1] || 'simple'
@@ -15,24 +16,9 @@ end
 text_provider = TextProvider.create source_method
 text = text_provider.text source
 
-if parse_method == 'simple'
-  links = []
-  start = 0
-  loop do
-    start = text.index('<a', start)
-    break unless start
-    start = text.index('href="', start)
-    next unless start
-    stop = text.index('"', start+6)
-    next unless stop
-    links.push text[start+6...stop]
-  end
-elsif parse_method == 'regexp'
-  links = text.scan(/<a.*?href="(.+?)[!"]/).flatten
-else
-  puts "Unknown parse method"
-  exit
-end
+#EXAM: parsing using a parametric factory DP
+link_parser = HTMLLinksParser.create(parse_method)
+links = link_parser.parse text
 
 #EXAM: display using the chain of responsability DP
 LinkListDisplayer.new
---
+++ html_links_parsers.rb
@@ -0,0 +1,38 @@
+class HTMLLinksParser
+    def self.create method
+        (case method
+        when 'simple'
+            WithSimple
+        when 'regexp'
+            WithRegexp
+        else
+            # raise "#{method} method is not supported"
+            puts "Unknown parse method"
+            exit
+        end).new
+    end
+    def initialize
+        raise "this class should not be instanciated"
+    end
+    class WithSimple
+        def parse text
+            links = []
+            start = 0
+            loop do
+                start = text.index('<a', start)
+                break unless start
+                start = text.index('href="', start)
+                next unless start
+                stop = text.index('"', start+6)
+                next unless stop
+                links.push text[start+6...stop]
+            end
+            links
+        end
+    end
+    class WithRegexp
+        def parse text
+            text.scan(/<a.*?href="(.+?)[!"]/).flatten
+        end
+    end
+end
